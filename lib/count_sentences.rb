91cbfd95ac65330e40571efca48d7b6e2718058a
Austin Johnson <technicallyaustin@gmail.com>
2020-01-11 19:56:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
+  
+ 
 
   def count_sentences
-
+   sentence_arr = []
+    period = sentence_arr.include?(".")
+    qstmrk = sentence_arr.include?("?") 
+    exclaim = sentence_arr.include?("!")
+  
+   sentence_arr << self.split(".")
+   sentence_arr << self.split("?")
+   sentence_arr << self.split("!")
+   
+   sentence_arr.each do |i|
+     i
+   end
+   sentence_arr.size
+  
   end
 end
